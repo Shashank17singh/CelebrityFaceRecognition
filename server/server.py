f2d9deb81a3136e180c7e8b92b76f31fb27d1ce0
Shashank17singh <152907288+Shashank17singh@users.noreply.github.com>
2026-06-17 14:40:08 +0530

    Add explicit static file route

--- server/server.py
+++ server/server.py
@@ -1,8 +1,6 @@
-import sys
-import os
+from flask import Flask, request, jsonify, render_template, send_from_directory
+import sys, os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
-from flask import Flask, request, jsonify, render_template
 import util
 
 app = Flask(__name__,
@@ -15,6 +13,10 @@ util.load_saved_artifacts()
 def index():
     return render_template('index.html')
 
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory(os.path.join(os.path.dirname(__file__), 'static'), filename)
+
 @app.route('/classify_image', methods=['POST'])
 def classify_image():
     image_data = request.form['image_data']
@@ -23,5 +25,4 @@ def classify_image():
     return response
 
 if __name__ == "__main__":
-    print("Starting Python Flask Server for Sports Celebrity Face Recognition")
     app.run(port=5000)
