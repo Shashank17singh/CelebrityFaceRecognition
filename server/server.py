fc5fd8a3dbf6c2816bf1dcd9cf28476a324ea71c
Shashank17singh <152907288+Shashank17singh@users.noreply.github.com>
2026-06-17 14:51:19 +0530

    Use /assets/ path for static files

--- server/server.py
+++ server/server.py
@@ -2,24 +2,19 @@ import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from flask import Flask, request, jsonify, render_template, send_file
+from flask import Flask, request, jsonify, render_template
 import util
 
 app = Flask(__name__,
             template_folder='templates',
             static_folder='static',
-            static_url_path='/static')
+            static_url_path='/assets')
 
 util.load_saved_artifacts()
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/static/images/<filename>')
-def serve_image(filename):
-    img_path = os.path.join(os.path.dirname(__file__), 'static', 'images', filename)
-    return send_file(img_path)
 
 @app.route('/classify_image', methods=['POST'])
 def classify_image():
